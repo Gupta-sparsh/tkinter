9a90df21465aefbae9815914043043b3bdb37f72
Sparsh Gupta <57013208+piratesparsh@users.noreply.github.com>
2020-09-05 14:41:04 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+#What this does is , it prints a string on the screen encapsulated in a windows.
+
+from tkinter import *
+root = Tk()
+myLabel = Label(root,text = "Hello Sparsh")
+myLabel.pack()
+root.mainloop()
+
+
